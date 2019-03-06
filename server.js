7e0962dd3fbdc6e07c1e4b83f18d999853dc3a7c
arsts <arstsu@gmail.com>
2019-03-06 20:52:42 +0300

    Modded signin route

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const bcrypt = require('bcrypt');
+const cors = require('cors');
 
 app.use(bodyParser.json())
+app.use(cors());
+
 const database = {
     users: [
         {
@@ -29,15 +33,21 @@ app.get('/', (req, res)=> {
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json('success')
-    } else {
-        res.status(400).json('error logging in');
-    }
+   // Load hash from your password DB.
+   bcrypt.compare(myPlaintextPassword, hash).then(function (res) {
+       // res == true
+   });
+   bcrypt.compare(someOtherPlaintextPassword, hash).then(function (res) {
+       // res == false
+   });
 })
 
 app.post('/register', (req, res) => {
-    const {email, name, password} = req.body
+    const {email, name, password} = req.body;
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+        console.log(hash);
+        
+    });
     database.users.push({
         id: '125',
         name: name,
@@ -49,14 +59,38 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length-1])
 })
 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+           found = true; 
+           return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found')
+    }
+})
 
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })  
+    if (!found) {
+        res.status(400).json('not found')
+    }
+})
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
 })
 
-// --> res = this is working
-// /signin --> POST = success/fail
-// /register --> POST = user 
-//  /profile/:userId --> GET = user 
-//  /image --> PUT --> user
+
+
