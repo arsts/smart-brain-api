8a4aa6fa9ce249bf570253cf4de4b0d961be0fb2
arsts <arstsu@gmail.com>
2019-03-19 21:34:45 +0300

    adding port

--- server.js
+++ server.js
@@ -33,19 +33,13 @@ app.get('/', (req, res)=> {
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-
 app.get('/profile/:id', (req, res) => { profile.handleProgileGet(req, res, db)})
-
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})  
-
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})  
- 
 
-
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
