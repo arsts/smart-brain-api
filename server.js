06445be066c3fefa298c69539b7c19de105499b8
arsts <arstsu@gmail.com>
2019-03-19 21:38:44 +0300

    changing response

--- server.js
+++ server.js
@@ -28,9 +28,7 @@ app.use(cors());
 
 
 
-app.get('/', (req, res)=> {
-    res.send(database.users)
-})
+app.get('/', (req, res)=> { res.send('it is working') })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
